7505392742f8088c7d2946c0ac795a8c03514f80
voa3hc <voa3hc@bosch.com>
2026-05-05 11:28:58 +0700

    Update run_auto_action.cmd

--- run_auto_action.cmd
+++ run_auto_action.cmd
@@ -12,28 +12,31 @@ REM Optional: activate virtual environment
 REM call venv\Scripts\activate
 
 :main
-	call :check_tini
-	if errorlevel 1 (
-		echo [W] Tool tini not found. Check python directly
-		call :check_python
-		if errorlevel 1 exit /b 1
-		pythonw auto_action.py
-	) else (
-		tini python=latest && pythonw auto_action.py exit /b 0
-	)
-	
+    call :check_tini
+    if errorlevel 1 (
+        echo [W] Tool tini not found. Check python directly
+        call :check_python
+        if errorlevel 1 exit /b 1
+        python auto_action.py
+    ) else (
+        call :run
+    )
+    exit 0
+
+:run
+    tini python=latest && python auto_action.py &
 
 :check_python
-	where python >nul 2>&1
-	if errorlevel 1 (
-		echo [E] Python not found.
-		echo     Install python from https://www.python.org/downloads/
-		pause
-		exit /b 1
-	)
-	echo [I] Python found
-	exit /b 0
+    where python >nul 2>&1
+    if errorlevel 1 (
+        echo [E] Python not found.
+        echo     Install python from https://www.python.org/downloads/
+        pause
+        exit /b 1
+    )
+    echo [I] Python found
+    exit /b 0
 
 :check_tini
-	where tini >nul 2>&1
-	exit /b errorlevel
+    where tini >nul 2>&1
+    exit /b errorlevel
